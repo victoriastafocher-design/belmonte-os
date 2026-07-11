444c98eb63b2e8dd54d015251f9f49ca26d7a9e8
victoriastafocher-design <victoriastafocher@gmail.com>
2026-07-10 22:02:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Belmonte OS iniciado.");
+const screen = document.getElementById("screen");
+
+screen.innerHTML = `
+    <h1>BELMONTE</h1>
+    <h2>OS</h2>
+    <p>Initializing...</p>
+`;
