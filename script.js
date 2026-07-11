2d987c3448c6de43e5f1cec4c1888e0615e94fa7
victoriastafocher-design <victoriastafocher@gmail.com>
2026-07-10 23:09:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,50 @@ function Boot()
 function Home()
 {
     screen.innerHTML = `
+
+        <h1>BELMONTE</h1>
+        <h2>OS</h2>
+
+
+        <div class="app-container">
+
+
+            <div class="card" onclick="OpenYoutube()">
+                <div class="icon">📺</div>
+                <div class="name">YouTube</div>
+            </div>
+
+
+            <div class="card">
+                <div class="icon">🌐</div>
+                <div class="name">Browser</div>
+            </div>
+
+
+            <div class="card">
+                <div class="icon">🎵</div>
+                <div class="name">Music</div>
+            </div>
+
+
+            <div class="card">
+                <div class="icon">⭐</div>
+                <div class="name">Favorites</div>
+            </div>
+
+
+            <div class="card">
+                <div class="icon">⚙</div>
+                <div class="name">Settings</div>
+            </div>
+
+
+        </div>
+
+    `;
+}
+{
+    screen.innerHTML = `
         <h1>BELMONTE TV</h1>
 
         <br>
