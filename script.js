30eaddac6e8b755b7cb0399d6cbf68cd4c7dbb9f
victoriastafocher-design <victoriastafocher@gmail.com>
2026-07-10 22:35:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ screen.innerHTML = `
     <h2>OS</h2>
     <p>Initializing...</p>
 `;
+
+setTimeout(() => {
+
+    screen.innerHTML = `
+        <iframe
+            width="960"
+            height="540"
+            src="https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1"
+            title="Belmonte Smart TV"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen>
+        </iframe>
+    `;
+
+}, 3000);
